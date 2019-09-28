eb39ef9ff3c48675b8dafba5b281b9649c951a41
jmshanahan <comeraghsolutions@gail.com>
2019-09-28 14:41:47 +0100

    Added webpack analyser and CDN for react and react-dom when in production

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component{
         const {count} = this.state;
         return (
         <div>
-        <h1>Hello world hot k</h1>
+        <h1>Hello world</h1>
             <h2 className={count > 10 ? 'warning': null}>Count: {this.state.count}</h2>
             <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
             <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
